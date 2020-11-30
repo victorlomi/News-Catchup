a5016888b4670a20493ef5667887733e415ddd02
plutos28 <gamevictor28@gmail.com>
2020-11-30 20:33:40 +0300

    create application factory and link to config

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, current_app
+from config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    return app
+
