c99dc74139aa1e1ae57a3e936c9b399678638acc
plutos28 <gamevictor28@gmail.com>
2020-12-01 15:02:54 +0300

    show the title of the news source in the source route

--- app/source/routes.py
+++ app/source/routes.py
@@ -1,8 +1,16 @@
 from flask import render_template, flash, redirect, url_for, request
 from app.source import bp
 
+def separate_names(name, character='-'):
+    """Split the names and return them as one string"""
+    names = name.split(character) 
+    final_name = ''
+    for s in names:
+        final_name += f'{s} '
+    return final_name.strip() 
+
 @bp.route('/source')
 def search_source():
     source = request.args.get('q') 
-    return f'your source is {source}'
+    return render_template('source.html', source=source, separate_names=separate_names) 
 
