edba0a60b3000901ad396b944e84030c601b6327
plutos28 <gamevictor28@gmail.com>
2020-12-01 17:12:50 +0300

    add api key and make request and expose reponse in template

--- app/source/routes.py
+++ app/source/routes.py
@@ -1,5 +1,7 @@
 import datetime
+import os
 from flask import render_template, flash, redirect, url_for, request
+import requests
 from app.source import bp
 
 def simplify_date(date):
@@ -7,13 +9,11 @@ def simplify_date(date):
 
 @bp.route('/source')
 def search_source():
-    article = {
-        "title": "Analysis: The fate of millions of Americans rests on Biden's economic team",
-        "description": "Joe Biden's incoming economic team will shoulder the desperate hopes of millions of Americans who have lost jobs, face eviction from their homes or who are going hungry in a monstrous Covid-19-induced slump.",
-        "url": "http://us.cnn.com/2020/12/01/politics/joe-biden-economic-team-white-house/index.html",
-        "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/201123153507-janet-yellen-super-tease.jpg",
-        "publishedAt": "2020-12-01T05:01:13Z",
-    }
-    source = request.args.get('q') 
-    return render_template('source.html', source=source, article=article, simplify_date=simplify_date) 
+    API_KEY = os.environ.get('API_KEY')
+    news_source = request.args.get('q')
+
+    url = f"https://newsapi.org/v2/top-headlines?sources={news_source}&apiKey={API_KEY}"
+    response = requests.get(url).json()
+    
+    return render_template('source.html', source=news_source, articles=response["articles"], simplify_date=simplify_date, api_key=API_KEY) 
 
