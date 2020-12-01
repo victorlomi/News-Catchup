005d9a3c8c7e50f0be5cfc74cf4de99ae715c819
plutos28 <gamevictor28@gmail.com>
2020-12-01 12:39:42 +0300

    add card information route to template and remove sources

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,16 +22,16 @@ def index():
 
     news_sources = {
         'bbc': source.Source('bbc-news', 'BBC News', images['bbc']), 
-        'bbcsports': source.Source('bbc-sport', 'BBC Sport', images['bbcsports']), 
-        'billboard': source.Source("buzzfeed", "Buzzfeed", images['billboard']), 
+        'bbc_sports': source.Source('bbc-sport', 'BBC Sport', images['bbcsports']), 
+        'billboard': source.Source("billboard", "Billboard", images['billboard']), 
         'bleacher_report': source.Source("bleacher-report", "Bleacher Report", images['bleacher_report']), 
         'bloomberg': source.Source("bloomberg", "Bloomberg", images['bloomberg']), 
         'business_insider': source.Source("business-insider", "Business Insider", images['business_insider']), 
         'buzzfeed': source.Source("buzzfeed", "Buzzfeed", images['buzzfeed']), 
-        'cbc-news': source.Source("cbc-news", "CBC News", images['cbc-news']), 
+        'cbc_news': source.Source("cbc-news", "CBC News", images['cbc-news']), 
         'cnn': source.Source("cnn", "CNN", images['cnn']), 
         'espn': source.Source("espn", "ESPN", images['espn']), 
         'fox': source.Source("fox-news", "Fox News", images['fox']), 
         'polygon': source.Source("polygon", "Polygon", images['polygon']), 
     }
-    return render_template('index.html') 
+    return render_template('index.html', news_sources=news_sources) 
