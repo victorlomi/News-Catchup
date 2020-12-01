736e8d865386438808671afa2f618a84f00e99a7
plutos28 <gamevictor28@gmail.com>
2020-12-01 12:21:27 +0300

    add homepage news source with ids, names, images

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,37 @@
 from flask import render_template, flash, redirect, url_for
 from app.main import bp
+from app.main import source
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    news_source = {
-        'name': 'vox news',
-        'description': 'The best news source for the woke millenial.',
-        'image': 'images/vox.jpg',
+    images = {
+        'bbc': 'images/bbc.png',
+        'bbcsports': 'images/bbcsports.jpg',
+        'billboard': 'images/billboard.jpg',
+        'bleacher_report': 'images/bleacher-report.jpg',
+        'bloomberg': 'images/bloomberg.png',
+        'business_insider': 'images/business-insider.png',
+        'buzzfeed': 'images/buzzfeed.png',
+        'cbc-news': 'images/cbc-news.jpg',
+        'cnn': 'images/cnn.jpg',
+        'espn': 'images/espn.png',
+        'fox': 'images/fox.jpg',
+        'polygon': 'images/polygon.png',
     }
-    return render_template('index.html', news_source=news_source) 
+
+    news_sources = {
+        'bbc': source.Source('bbc-news', 'BBC News', images['bbc']), 
+        'bbcsports': source.Source('bbc-sport', 'BBC Sport', images['bbcsports']), 
+        'billboard': source.Source("buzzfeed", "Buzzfeed", images['billboard']), 
+        'bleacher_report': source.Source("bleacher-report", "Bleacher Report", images['bleacher_report']), 
+        'bloomberg': source.Source("bloomberg", "Bloomberg", images['bloomberg']), 
+        'business_insider': source.Source("business-insider", "Business Insider", images['business_insider']), 
+        'buzzfeed': source.Source("buzzfeed", "Buzzfeed", images['buzzfeed']), 
+        'cbc-news': source.Source("cbc-news", "CBC News", images['cbc-news']), 
+        'cnn': source.Source("cnn", "CNN", images['cnn']), 
+        'espn': source.Source("espn", "ESPN", images['espn']), 
+        'fox': source.Source("fox-news", "Fox News", images['fox']), 
+        'polygon': source.Source("polygon", "Polygon", images['polygon']), 
+    }
+    return render_template('index.html') 
---
+++ app/main/source.py
@@ -0,0 +1,8 @@
+class Source():
+    """Keep track of the properties of a news source"""
+
+    def __init__(self, id, name, image):
+        self.id = id
+        self.name = name
+        self.image = image
+    
