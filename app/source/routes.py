a5b20a8318353dab9e5a4683d4eee4f0be933255
plutos28 <gamevictor28@gmail.com>
2020-12-01 16:34:18 +0300

    add mock article and style it in sources page

--- app/source/routes.py
+++ app/source/routes.py
@@ -1,16 +1,19 @@
+import datetime
 from flask import render_template, flash, redirect, url_for, request
 from app.source import bp
 
-def separate_names(name, character='-'):
-    """Split the names and return them as one string"""
-    names = name.split(character) 
-    final_name = ''
-    for s in names:
-        final_name += f'{s} '
-    return final_name.strip() 
+def simplify_date(date):
+    return date[0:10]
 
 @bp.route('/source')
 def search_source():
+    article = {
+        "title": "Analysis: The fate of millions of Americans rests on Biden's economic team",
+        "description": "Joe Biden's incoming economic team will shoulder the desperate hopes of millions of Americans who have lost jobs, face eviction from their homes or who are going hungry in a monstrous Covid-19-induced slump.",
+        "url": "http://us.cnn.com/2020/12/01/politics/joe-biden-economic-team-white-house/index.html",
+        "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/201123153507-janet-yellen-super-tease.jpg",
+        "publishedAt": "2020-12-01T05:01:13Z",
+    }
     source = request.args.get('q') 
-    return render_template('source.html', source=source, separate_names=separate_names) 
+    return render_template('source.html', source=source, article=article, simplify_date=simplify_date) 
 
